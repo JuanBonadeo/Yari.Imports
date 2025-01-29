80c4f10caee06f4c0b27224498a5ddf101548809
Juan Cruz <juancruzbonadeo04@gmail.com>
2025-01-29 16:26:15 -0300

    black

--- src/ui/Logo/Logo.jsx
+++ src/ui/Logo/Logo.jsx
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom';
 export const Logo = (prop) => {
   return (
     <Link to="/" className='logo'>
-      {/* <img className={prop.className} src="logo.png" alt="Oud parfume" /> */}
-      <h1 className=''>YARI.IMPORT</h1>
+      <img className={prop.className} src="logo.png" alt="Oud parfume" />
+      {/* <h1 className=''>YARI.IMPORT</h1> */}
     </Link>
   );
 };
